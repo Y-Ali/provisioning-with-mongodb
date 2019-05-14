40ada0d4dd2e8b2ef72e359528ab90fba9c0a1e0
Tech-A29 <tech-a29@tech-a62s-air.home>
2019-05-14 21:35:15 +0100

    provisioning with mongodb - atempt 1

---
+++ Vagrantfile
@@ -0,0 +1,11 @@
+Vagrant.configure("2") do |config|
+  config.vm.box ="ubuntu/xenial64"
+  config.vm.network("private_network", ip: "192.168.10.100")
+  config.hostsupdater.aliases = ["development.local"]
+
+
+  #config.vm.synced_folder("mongodb","/mongodb")
+
+  config.vm.provision("shell", path: "environment/provision.sh")
+
+end
---
+++ environment/provision.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+
+sudo apt-get update
+
+
+sudo apt-get install -y mongodb-org=4.0 mongodb-org-server=4.0 mongodb-org-shell=4.0 mongodb-org-mongos=4.0 mongodb-org-tools=4.0
+
+#sudo apt-get install -y mongodb-org
+
+sudo service mongodb start
